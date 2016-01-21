6f87ecdbf8eac10571701d899230699da13b8256
root <root@romulus-laptop.(none)>
2016-01-21 22:35:28 +0800

    submitter: sunzhao
    Remove compile file.

--- myproject/examples/scull/.scull.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/myproject/examples/scull/scull.o := ld -m elf_i386   -r -o /root/myproject/examples/scull/scull.o /root/myproject/examples/scull/main.o /root/myproject/examples/scull/pipe.o /root/myproject/examples/scull/access.o 
--- myproject/examples/scull/.tmp_versions/scull.mod
+++
@@ -1,2 +0,0 @@
-/root/myproject/examples/scull/scull.ko
-/root/myproject/examples/scull/main.o /root/myproject/examples/scull/pipe.o /root/myproject/examples/scull/access.o
